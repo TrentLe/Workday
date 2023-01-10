aacb8ba0ff69c1122a32d1407accc702ed4d4be1
unknown <raeburn.trent6@gmail.com>
2023-01-10 02:47:12 -0500

    cleaned code

--- assets/script.js
+++ assets/script.js
@@ -3,16 +3,16 @@ var row = $('.row');
 var past = $('.past');
 var present = $('.present');
 var future = $('.future');
+var block = $('.time-block');
 
-// Current time
+
+// Current date and time
 $(document).ready(function(){
-  var today = new Date();
-  var tDate = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-  var cTime = today.getHours() + ":" + today.getMinutes();
-  var date = tDate + ' | ' + cTime;
-  $('#currentDay').text(date)
+ 
+  var today = dayjs().format('MMM-DD-YYYY | h:mm');
+  $('#currentDay').text(today)
 
-})
+});
 
 $(function () {
     // TODO: Add a listener for click events on the save button. This code should
@@ -22,16 +22,31 @@ $(function () {
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
     //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
     //
-    // TODO: Add code to display the current date in the header of the page.
+
+    // Function to set the colors of the time blocks
+    block.each(function(){
+      var pTime = $(this).parents('row');
+      var fTime = parseInt(pTime.attr('id'));
+      var cTime = parseInt(dayjs().format('H'));
+
+      if (fTime < cTime){
+        $(this).addClass('past');
+      } else if (fTime === cTime){
+        $(this).addClass('present')
+        $(this).removeClass('past')
+      } else {
+        $(this).addClass('future')
+        $(this).removeClass('past')
+        $(this).removeClass('present')
+      }
+    })
+
+
+    
+
   });
   
